c44f101be735c2e523c8a99bf3657c76fd5622c4
bil <nabilsyawaludin108@gmail.com>
2024-09-12 12:41:49 +0700

    .

--- p2/src/index.js
+++ p2/src/index.js
@@ -6,6 +6,17 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+
+app.get('/about', (req, res) => 
+    {res.send('Halaman tentang perusahaan');
+    
+});
+
+app.get('/contact', (req, res) => 
+    {res.send('Kontak perusahaan');
+    
+});
+
 app.listen(port, () => {
-  console.log(`Example app listening on port 3000 ${port}`)
+  console.log(`Example app listening on port ${port}`)
 })
